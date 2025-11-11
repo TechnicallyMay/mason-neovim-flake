4249142bf4c8e9b08dc979921e200e24e1069bcf
TechnicallyMay <masonwells01@gmail.com>
2025-11-11 15:10:46 -0700

    Fix cmp

--- config/default.nix
+++ config/default.nix
@@ -1,12 +1,13 @@
 {
   imports = [
+    ./cmp.nix
     ./colors.nix
+    ./general-keymaps.nix
     ./lightline.nix
     ./lsp.nix
     ./oil.nix
     ./settings.nix
     ./telescope.nix
     ./treesitter.nix
-    ./general-keymaps.nix
   ];
 }
