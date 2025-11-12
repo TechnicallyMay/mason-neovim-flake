10e692d108c38b1bc3054d2f09a008ee7abf1994
TechnicallyMay <masonwells01@gmail.com>
2025-11-11 17:54:33 -0700

    LSP Keymaps

--- config/lsp.nix
+++ config/lsp.nix
@@ -7,5 +7,49 @@
       lua_ls.enable = true;
       nixd.enable = true;
     };
+
+    keymaps = {
+      silent = true;
+      lspBuf = {
+        gd = {
+          action = "definition";
+          desc = "Goto Definition";
+        };
+        gr = {
+          action = "references";
+          desc = "Goto References";
+        };
+        gi = {
+          action = "implementation";
+          desc = "Goto Implementation";
+        };
+        K = {
+          action = "hover";
+          desc = "Hover";
+        };
+        "<leader>ss" = {
+          action = "workspace_symbol";
+          desc = "Workspace Symbol";
+        };
+        "<leader>rn" = {
+          action = "rename";
+          desc = "Rename";
+        };
+      };
+      diagnostic = {
+        "<leader>sd" = {
+          action = "open_float";
+          desc = "Line Diagnostics";
+        };
+        "[d" = {
+          action = "goto_next";
+          desc = "Next Diagnostic";
+        };
+        "]d" = {
+          action = "goto_prev";
+          desc = "Previous Diagnostic";
+        };
+      };
+    };
   };
 }
