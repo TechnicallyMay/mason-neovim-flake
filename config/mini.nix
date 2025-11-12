1980b3cc0f796526cc59348fae0db16352984c10
TechnicallyMay <masonwells01@gmail.com>
2025-11-12 08:16:48 -0700

    mini.surround  keymaps

--- config/mini.nix
+++ config/mini.nix
@@ -3,9 +3,20 @@
     enable = true;
     modules = {
       ai.enable = true;
-      surround.enable = true;
       move.enable = true;
       splitjoin.enable = true;
+      surround = {
+        enable = true;
+        mappings = {
+          add = "gsa";
+          delete = "gsd";
+          find = "gsf";
+          find_left = "gsF";
+          highlight = "gsh";
+          replace = "gsr";
+          update_n_lines = "gsn";
+        };
+      };
     };
   };
 }
