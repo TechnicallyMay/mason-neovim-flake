e522d61647da292ae19f2cab23071121b101f983
TechnicallyMay <masonwells01@gmail.com>
2025-11-12 21:59:17 -0700

    Add keymap to remove highlight search

--- config/general-keymaps.nix
+++ config/general-keymaps.nix
@@ -1,3 +1,12 @@
 { ... }:
 {
+  keymaps = [
+    {
+      key = "<Esc>";
+      action.__raw = "vim.cmd.noh";
+      "mode" = "n";
+      options.desc = "Remove any highlighted matches";
+      options.silent = true;
+    }
+  ];
 }
