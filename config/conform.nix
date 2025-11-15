fc4dca83a10bdda0d434712700df9eef0f28a6de
TechnicallyMay <masonwells01@gmail.com>
2025-11-14 21:56:47 -0700

    bugfix: Update gdscript formatter filetype

--- config/conform.nix
+++ config/conform.nix
@@ -32,19 +32,10 @@
           command = lib.getBin pkgs.gdtoolkit_4 + "/bin/gdformat";
         };
       };
-      format_on_save =
-        # Lua
-        ''
-          function(bufnr)
-            local function on_format(err)
-              if err and err:match("timeout$") then
-                slow_format_filetypes[vim.bo[bufnr].filetype] = true
-              end
-            end
-
-            return { timeout_ms = 200, lsp_fallback = true }, on_format
-           end
-        '';
+      format_on_save = {
+        timeout_ms = 200;
+        lsp_fallback = true;
+      };
     };
   };
 }
