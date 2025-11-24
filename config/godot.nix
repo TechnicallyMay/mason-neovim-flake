715a415078ccd9cff1092062315a4896cfeb618f
TechnicallyMay <masonwells01@gmail.com>
2025-11-23 17:14:42 -0700

    Better godot integration

--- config/godot.nix
+++ config/godot.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   lsp.servers.gdscript = {
     enable = true;
     package = null;
@@ -10,5 +9,27 @@
     godot_resource
     gdshader
   ];
-}
 
+  autoCmd = [
+    {
+      event = [
+        "VimEnter"
+        "DirChanged"
+      ];
+      callback.__raw = ''
+        function()
+          local cwd = vim.fn.getcwd()
+          if not vim.uv.fs_stat(cwd .. "/project.godot") then
+            return
+          end
+
+          local is_server_running = vim.uv.fs_stat(cwd .. "/.server.pipe")
+          if not is_server_running then
+            print("Detected a godot project, starting a Neovim server.")
+            vim.fn.serverstart(cwd .. "/.server.pipe")
+          end
+        end
+      '';
+    }
+  ];
+}
