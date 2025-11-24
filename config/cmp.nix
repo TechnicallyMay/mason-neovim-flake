09c156e8a5a56e8f438c0b107a39047798a6835f
TechnicallyMay <masonwells01@gmail.com>
2025-11-23 17:25:55 -0700

    Snippets

--- config/cmp.nix
+++ config/cmp.nix
@@ -1,8 +1,16 @@
 {
+  plugins.friendly-snippets = {
+    enable = true;
+  };
   plugins.blink-cmp = {
     enable = true;
     settings = {
-      sources.default = ["lsp" "path" "snippets" "buffer"];
+      sources = {
+        default = ["lsp" "path" "snippets" "buffer"];
+        providers.snippets.score_offset = 10;
+        providers.path.score_offset = -1;
+        providers.buffer.score_offset = -10;
+      };
       signature.enabled = true;
     };
   };
