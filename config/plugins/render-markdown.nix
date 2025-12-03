7d995edf7bc9749578264cb55d81463e51aadfce
TechnicallyMay <masonwells01@gmail.com>
2025-12-02 18:38:41 -0700

    Nicer markdown indentation

--- config/plugins/render-markdown.nix
+++ config/plugins/render-markdown.nix
@@ -1,6 +1,11 @@
 {
   plugins.render-markdown = {
     enable = true;
-    settings.completions.lsp.enabled = true;
+    settings = {
+      completions.lsp.enabled = true;
+      heading.border = true;
+      indent.enabled = true;
+      indent.skip_heading = true;
+    };
   };
 }
